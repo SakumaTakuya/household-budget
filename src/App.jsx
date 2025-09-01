184f53d538882170a2eb568ad633abe5d659f842
SakumaTakuya <mo.takusakkun@gmail.com>
2025-09-01 17:50:34 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>かんたん家計簿</h1>
+      <p>ここから開発スタート！</p>
+      {/* 収支サマリー */}
+      {/* 入力フォーム */}
+      {/* 取引履歴リスト */}
+    </div>
+  );
+}
+
+export default App;
