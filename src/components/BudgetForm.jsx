735a5a90b3e4e42318f374c469629e0e9f215fc9
1sh1d4 <isd97cd8adb@icloud.com>
2025-09-06 19:00:46 +0900

    Firebaseを使用して予算を追加する機能を実装

--- src/components/BudgetForm.jsx
+++ src/components/BudgetForm.jsx
@@ -1,9 +1,85 @@
+import { useState } from "react";
 import './BudgetForm.css';
+import { addDoc, collection, } from "firebase/firestore/lite";
+import { db } from "../Firebase";
 
-function BudgetForm() {
+const BudgetForm = () => {
+  const [budget, setBudget] = useState({
+    date: "",
+    money: "",
+    category: "",
+    memo: ""
+  });
+
+  const handleBudget = (e) => {
+    setBudget((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  }
+
+  const createBudget = async (event) => {
+    event.preventDefault();
+    try {
+      await addDoc(collection(db, "budget"), budget);
+      alert("追加完了しました");
+      setBudget({
+        date: "",
+        money: "",
+        category: "",
+        memo: ""
+      });
+    } catch (error) {
+      console.error(error);
+    };
+  };
+
   return (
     <div className="budget-form">
-      {/* コンポーネント内容 */}
+      <div className="date-form">
+        <label htmlFor="date">日付</label>
+        <input
+          name="date"
+          value={budget.date}
+          onChange={handleBudget}
+          type="date"
+          id="date"
+        />
+      </div>
+      <div className="money-form">
+        <label htmlFor="money">金額</label>
+        <input
+          name="money"
+          value={budget.money}
+          onChange={handleBudget}
+          type="number"
+          id="money"
+        />
+      </div>
+      <div className="category-name">
+        <label htmlFor="category">カテゴリー</label>
+        <select
+          name="category"
+          value={budget.category}
+          onChange={handleBudget}
+          id="category">
+          <option value="">カテゴリーを選択してください</option>
+          <option value="食費">食費</option>
+          <option value="光熱費">光熱費</option>
+          <option value="家賃">家賃</option>
+          <option value="交際費">交際費</option>
+          <option value="日用品費">日用品費</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="memo">メモ</label>
+        <textarea
+          name="memo"
+          value={budget.memo}
+          onChange={handleBudget}
+          id="memo">
+        </textarea>
+      </div>
+      <div>
+        <button onClick={createBudget}>追加する</button>
+      </div>
     </div>
   );
 }
